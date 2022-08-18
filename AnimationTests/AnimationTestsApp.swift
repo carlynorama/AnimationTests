1e2fe47a9e659a5bccd2e3f1e1b0140f2fb412b7
carlynorama <carlynorama@gmail.com>
2022-08-18 07:27:00 -0700

    Initial Commit

---
+++ AnimationTests/AnimationTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestsApp.swift
+//  AnimationTests
+//
+//  Created by Labtanza on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
